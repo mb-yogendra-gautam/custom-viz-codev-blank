9d10f3b45da2fe15cc76d0c8d095ec2c38cbc7e0
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2023-10-17 06:52:07 -0400

    init

--- src/customVis.js
+++ src/customVis.js
@@ -1,6 +1,17 @@
 looker.plugins.visualizations.add({
   // The create method gets called once on initial load of the visualization.
   // It's just a convenient place to do any setup that only needs to happen once.
+
+//   options: {
+//   titleText: {
+//     type: "string",
+//     label: "Title Text",
+//     default: "Dead Stage",
+//     display: "text",
+//     placeholder: "Dead Stage",
+//     section: "Title",
+//   }
+// },
   create: function (element, config) {},
 
   // The updateAsync method gets called any time the visualization rerenders due to any kind of change,
@@ -8,8 +19,10 @@ looker.plugins.visualizations.add({
   updateAsync: function (data, element, config, queryResponse, details, done) {
     element.innerHTML = "";
     const div = document.createElement("div");
-    div.innerText = "Custom vis!";
+    div.innerText = "Elizabeth!";
     element.appendChild(div);
     done();
+
+
   },
 });
